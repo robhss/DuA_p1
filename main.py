92361decc9f088913300d8c18505af15dfb6e89d
Robin <robin.hess@outlook.com>
2023-05-13 11:54:57 +0200

    rename test data

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import sys
 arg = sys.argv              #list of cmd-line arguments
 alg = arg[1]
 
+print(arg[2])
 with open(arg[2]) as f:
     list = f.readlines
+f.close
+
+for i in list:
+    print[list[i]]
+
