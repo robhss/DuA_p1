07e93eec1eb8b16de8fc5924ccdd47500acc3fb4
Robin <robin.hess@outlook.com>
2023-05-14 11:44:21 +0200

    rename test folder

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ arg = sys.argv              #list of cmd-line arguments
 alg = arg[1]
 
 print(arg[2])
-with open(arg[2]) as f:
+with open(arg[2],'r') as f:
     list = f.readlines
-f.close
 
 for i in list:
     print[list[i]]
