ebfa41144486c0c2fbd90cbad73b6481912523d5
Robin <robin.hess@outlook.com>
2023-05-14 12:21:38 +0200

    namensausgabe raw string problem fixed

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 arg = sys.argv              #list of cmd-line arguments
 alg = arg[1]
 
-print(arg[2])
-with open(arg[2],'r') as f:
-    list = f.readlines
+#p = r"C:\Users\Robin\Documents\DuA_Pra\DuA_p1\Test-data\a0020.txt"
+with open(fr"{arg[2]}",'r') as f:
+    list = f.readlines()
 
 for i in list:
-    print[list[i]]
+    print(i)
 
