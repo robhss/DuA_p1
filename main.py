323d92f17decf1715a71ee104d6f2caabad90e2d
Robin <robin.hess@outlook.com>
2023-05-14 14:30:37 +0200

    2 dimensionale liste mit vornamen und namen

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ arg = sys.argv              #list of cmd-line arguments
 alg = arg[1]
 
 #p = r"C:\Users\Robin\Documents\DuA_Pra\DuA_p1\Test-data\a0020.txt"
+#with open(p,'r') as f:
+#    list = f.readlines()
 with open(fr"{arg[2]}",'r') as f:
     list = f.readlines()
 
+unsorted = []
+
 for i in list:
-    i = i[:-1]
-    print(i)
+    i = i[:-1]                            
+    unsorted.append([i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])                                   #erzeugt 2 dimensionale Liste mit vornamen und namen
+    
+print(unsorted)
+
 
