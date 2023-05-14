5ae839e349fc033542e1fc13e1391432badaf1c6
Robin <robin.hess@outlook.com>
2023-05-14 15:58:53 +0200

    argument zur auswahl von sort alg

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ for i in list:
     i = i[:-1]                            
     unsorted.append([i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])                                   #erzeugt 2 dimensionale Liste mit vornamen und namen
     
-print(unsorted)
+sorted = []
+if arg[1] is "-mergesort":
+    #sorted = mergesort(unsorted)
+    pass
+elif arg[1] is "-quicksort":
+    #sorted = quicksort(unsorted)
+    pass
 
 
