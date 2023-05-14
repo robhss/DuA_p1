bb9b790e5513f4cc31a365eb1e8eaf7927f11bca
Robin <robin.hess@outlook.com>
2023-05-14 17:36:33 +0200

    mergesort fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from mergeSort import mergesort
+from compare import compareData
 
 arg = sys.argv  # list of cmd-line arguments
 alg = arg[1]
@@ -14,18 +15,23 @@ unsorted = []
 
 for i in liste:
     i = i[:-1]
-    unsorted.append(
-        [i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])  # erzeugt 2 dimensionale Liste mit vornamen und namen
+    unsorted.append([i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])  # erzeugt 2 dimensionale Liste mit vornamen und namen
 
-sortiert = []
+sorted = []
 if arg[1] == "-mergesort":
-    sortiert = mergesort(unsorted)
+    sorted = mergesort(unsorted)
 elif arg[1] == "-quicksort":
     # sorted = quicksort(unsorted)
     pass
 
-s = str
-for i in sortiert:
+s = ''
+for i in sorted:
     s = "{p1}{p2} {p3}\n".format(p1=s, p2=i[0], p3=i[1])
 
+#print(sorted)
 print(s)
+
+if sorted == compareData("{p1}.sol".format(p1 = arg[2])):
+    print(True)
+
+#print(compareData("{p1}.sol".format(p1 = arg[2])))
