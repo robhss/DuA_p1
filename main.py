ae8614a0efa0528c8dcf08cf4d634fed88d99a99
TheDarkFighter <24478096+TheDarkFighter@users.noreply.github.com>
2023-05-14 17:19:00 +0200

    Umbennenung um schon benutzte Namen zu vermeiden

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import sys
 from mergeSort import mergesort
 
-arg = sys.argv              #list of cmd-line arguments
+arg = sys.argv  # list of cmd-line arguments
 alg = arg[1]
 
-#p = r"C:\Users\Robin\Documents\DuA_Pra\DuA_p1\Test-data\a0020.txt"
-#with open(p,'r') as f:
+# p = r"C:\Users\Robin\Documents\DuA_Pra\DuA_p1\Test-data\a0020.txt"
+# with open(p,'r') as f:
 #    list = f.readlines()
-with open(fr"{arg[2]}",'r') as f:
-    list = f.readlines()
+with open(fr"{arg[2]}", 'r') as f:
+    liste = f.readlines()
 
 unsorted = []
 
-for i in list:
-    i = i[:-1]                            
-    unsorted.append([i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])                                   #erzeugt 2 dimensionale Liste mit vornamen und namen
-    
-sorted = []
+for i in liste:
+    i = i[:-1]
+    unsorted.append(
+        [i[:-(len(i) - i.index(" "))], i[i.index(" ") + 1:]])  # erzeugt 2 dimensionale Liste mit vornamen und namen
+
+sortiert = []
 if arg[1] == "-mergesort":
-    sorted = mergesort(unsorted)
+    sortiert = mergesort(unsorted)
 elif arg[1] == "-quicksort":
-    #sorted = quicksort(unsorted)
+    # sorted = quicksort(unsorted)
     pass
 
 s = str
-for i in sorted:
-
-    s = "{p1}{p2} {p3}\n".format(p1 = s, p2 = i[0], p3 = i[1])
+for i in sortiert:
+    s = "{p1}{p2} {p3}\n".format(p1=s, p2=i[0], p3=i[1])
 
 print(s)
